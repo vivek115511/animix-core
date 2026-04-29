1500006dcb49df97284cd2567e6252cb98723b52
vivek115511 <nirmalamarathe1@gmail.com>
2026-04-29 17:05:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Request, UploadFile, File, Form, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from moviepy import VideoFileClip
+from moviepy.editor import VideoFileClip
 
 app = FastAPI()
 
